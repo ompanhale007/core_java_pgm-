806a5e92833b6610dbc8369d2b1470dc52259c71
ompanhale007 <ompanhale007@gmail.com>
2023-09-12 10:07:43 +0530

    new branch created

--- ifelsestatement/sample2.java
+++ ifelsestatement/sample2.java
@@ -5,5 +5,6 @@ public class sample2
     public static void main(String[] args) {
         System.out.println("git sample 2");
 	System.out.println("git sample 3");
+    System.out.println("git branch created");
     }
 }
